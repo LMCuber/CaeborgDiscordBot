3ef58842544e4d01cbf1e9fe991be094658f7163
Leonik <leonik@Leoniks-MBP.home>
2023-04-14 17:51:43 +0200

    removed junk

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ async def kick(ctx, user: discord.Member, reason):
 # commands available to everyone
 @bot.command(brief="Shh...")
 async def rickroll(ctx):
-    await ctx.send(file=discord.File(path("assets", "rickroll.gif")))
+    await ctx.send(file=discord.File(path(f"{root}assets", "rickroll.gif")))
 
 @bot.command(brief="Calls this command")
 async def help(ctx, strcommand=None):
